7f56a16421278868c6f1636fefb461aa865d39a4
Shanil97 <46552983+Shanil97@users.noreply.github.com>
2020-06-01 13:14:12 +0530

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,26 @@
+package lk.ac.kln.fct.learn.reflection.main;
+
+import java.lang.reflect.InvocationTargetException;
+import lk.ac.kln.fct.learn.reflection.shapes.Rectangle;
+import java.lang.reflect.Field;
+
+public class Main {
+	public static void main(String[] args) {
+		Rectangle rectangle = null;				
+		try {
+			Class<?> clazz = Rectangle.class;
+			rectangle = (Rectangle) clazz.getConstructor().newInstance();
+			//Get the super class's name field.
+			//Set the value "Rectangle" to name field.
+			Field field =clazz.getSuperclass().getDeclaredField("name");
+			field.setAccessible(true);
+			field.set(rectangle,"Rectangle");
+
+
+
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}
+		System.out.println("Shape name: "+ rectangle.getName());
+	}
+}
